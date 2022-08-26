14548750ec6327185f255291f4de5df7158d7508
Danish-mohammed <matifs.1992@gmail.com>
2022-08-26 17:01:36 +0530

    Completed Uc2

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -3,11 +3,16 @@ class Test{
     public void add(){
         System.out.println("Adding 2 number");
     }
+
+    public void acceptName(){
+        System.out.println("Accepting user name");
+    }
 }
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and ladder problem");
         Test test=new Test();
         test.add();
+        test.acceptName();
     }
 }
