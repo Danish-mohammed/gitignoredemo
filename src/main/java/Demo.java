274c4965ef9d51df2850a04c265e0f5195640eb6
Danish-mohammed <matifs.1992@gmail.com>
2022-08-26 16:56:56 +0530

    Completed UC1

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,5 +1,13 @@
+
+class Test{
+    public void add(){
+        System.out.println("Adding 2 number");
+    }
+}
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and ladder problem");
+        Test test=new Test();
+        test.add();
     }
 }
