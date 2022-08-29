a5870d28050ee302f633a21cb475683603b8d9bf
Danish-mohammed <matifs.1992@gmail.com>
2022-08-29 17:08:10 +0530

    First commit

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,11 +1,41 @@
 
 class Test{
+
+    private int a;
+    private  int b;
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "a=" + a +
+                ", b=" + b +
+                '}';
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
     public void add(){
         System.out.println("Adding 2 number");
     }
 
     public void acceptName(){
         System.out.println("Accepting user name");
+        int rand = (int) ((Math.random()*6)+1);
+        System.out.println(rand);
     }
 }
 public class Demo {
